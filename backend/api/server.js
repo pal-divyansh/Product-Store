5c2491274fe7ea2120f144643475229586b55d4b
pal-divyansh <paldivyansh25@gmail.com>
2025-04-18 01:55:23 +0530

    Edit Mongo_url

--- backend/api/server.js
+++ backend/api/server.js
@@ -18,9 +18,9 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
+const PORT = process.env.PORT || 5000;
 
-
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDb();
     console.log("Server started at http://localhost:5000");
 });
